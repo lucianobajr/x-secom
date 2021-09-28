eed0aa40a40cc880a31a9305a17a22c26e866165
Luciano Belo <luciano.alcantara@ufv.br>
2021-09-28 09:22:13 -0300

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const routes = Router();
 const categories = require("./categories");
 const products = require("./products");
 
+routes.get("/", (req, res) => res.json({ ok: "api is running! =)" }));
+
 routes.use("/categories", categories);
 routes.use("/products", products);
 
